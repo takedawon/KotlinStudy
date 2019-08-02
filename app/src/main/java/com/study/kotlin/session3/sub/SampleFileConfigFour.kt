c3cd7825a015a59ea5eb6e253ca42f8c1e8d9572
takedawon <takedawon@naver.com>
2019-08-02 16:08:05 +0900

    3-2-5 이름 변경해서 임포트하기

--- app/src/main/java/com/study/kotlin/session3/sub/SampleFileConfigFour.kt
+++ app/src/main/java/com/study/kotlin/session3/sub/SampleFileConfigFour.kt
@@ -1,9 +1,17 @@
 package com.study.kotlin.session3.some_package
 
+import android.os.health.SystemHealthManager
+import java.util.Date
+import java.sql.Date as SqlDate
+
 var fourNum = 10
 fun four() {
 
 }
-class SampleFileConfigFour {
+fun main(args: Array<String>) {
+    val date = Date()
+    val date2 = SqlDate(System.currentTimeMillis())
 
+    println("java.util.Date() : " + date)
+    println("java.sql.Date() : " + date2)
 }
