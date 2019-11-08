e74ed134f99eb2b397972557e01e771be432223e
takedawon <takedawon@naver.com>
2019-11-08 17:24:07 +0900

    6-3-5 증감 연산자

---
+++ app/src/main/java/com/study/kotlin/session6/SampleIncrementActivity.kt
@@ -0,0 +1,9 @@
+package com.study.kotlin.session6
+
+class SampleIncrementActivity {
+    constructor() {
+        var data = 10
+        var result = data++
+        println("result = $result, data = $data")
+    }
+}
