f1bcfab44c8593955d202fd9a7de4d03e6513649
takedawon <takedawon@naver.com>
2019-11-08 17:17:11 +0900

    6-3-4 복합 대입 연산자

--- app/src/main/java/com/study/kotlin/session11/SampleDataClassActivity.kt
+++ app/src/main/java/com/study/kotlin/session11/SampleDataClassActivity.kt
@@ -4,8 +4,29 @@ package com.study.kotlin.session11
 //코틀린에선 이런 클래스들을 조금 더 편하게 이용하기 위해 데이터 클래스를 제공한다.
 
 data class User(val name:String, var age:Int)
+class Person(val name:String, var age:Int)
 
 // 주 생성자를 선언해야 하며 주 생성자의 매개변수는 최소 하나 이상이어야 한다.
 // 모든 주 생성자의 매개변수는 var 혹은 val로 선언해야 한다.
 // 데이터 클래스 abstract, open, sealed, inner 등의 예약어를 추가할 수 없다.
 
+// 데이터 클래스가 유용한 이유는 다양한 기능의 함수를 제공하기 때문.
+// 개발자가 클래스 내에 선언하지 않아도 자동으로 다음의 함수를 제공함
+// equals() / hashCode()
+// toString()
+// componentN()
+// copy()
+
+// equals() 는 객체의 데이터가 같은지를 비교할 때 사용.
+
+class SampleDataClassActivity() {
+    val user = User("lanic", 25)
+    val user2 = User("lanic", 25)
+
+
+    fun dataPrint() {
+        print(user.equals(user2))
+    }
+
+
+}
---
+++ app/src/main/java/com/study/kotlin/session6/SampleCompoundAssignActivity.kt
@@ -0,0 +1,17 @@
+package com.study.kotlin.session6
+
+class SampleCompoundAssignActivity {
+    constructor() {
+        var data=10
+        data+=5
+        print(data)
+        data-=5
+        print(data)
+        data*=5
+        print(data)
+        data/=2
+        print(data)
+        data%=5
+        print(data)
+    }
+}
